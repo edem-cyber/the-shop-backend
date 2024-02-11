89f3f7c7e0b13fd33c90c025847ac00495135767
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-11 20:23:12 +0000

    Add route to get all users

--- routes/users.ts
+++ routes/users.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import { createResponse } from '../utils/helper';
 
 const prisma = new PrismaClient();
 
@@ -15,10 +16,33 @@ router.get('/getUserByEmail', async (req: Request, res: Response) => {
             },
         });
 
-        res.status(200).json(user);
+
     } catch (error: any) {
         console.log(error);
     }
 });
 
+
+// get all users
+router.get('/users', async (req: Request, res: Response) => {
+    try {
+        const users = await prisma.user.findMany();
+        const count = await prisma.user.count();
+
+        createResponse(
+            {
+                res,
+                statusCode: 200,
+                statusText: 'OK',
+                message: 'User found',
+                data: {
+                    count,
+                    users,
+                },
+            }
+        );
+    } catch (error: any) {
+        console.log(error);
+    }
+});
 module.exports = router;
