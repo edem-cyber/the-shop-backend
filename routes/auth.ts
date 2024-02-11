179c30aca00e1bf440bf5fed89c7c689fbaf9d3e
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-11 20:23:01 +0000

    Refactor variable names for clarity and consistency

--- routes/auth.ts
+++ routes/auth.ts
@@ -8,6 +8,7 @@ import { CreateuserProps } from "../types/createuserprops";
 import Joi from "joi";
 import createHttpError from "http-errors";
 import { createResponse } from "../utils/helper";
+import { stat } from "fs";
 
 
 
@@ -17,11 +18,150 @@ const prisma = new PrismaClient();
 const router = Router();
 module.exports = router;
 
-export const createUserFn = async (props: CreateuserProps) => {
-    const { email, password, username, firstName, lastName, phoneNumber, bio } = props;
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+const signUpSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    username: Joi.string().min(3).required(),
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
+    phoneNumber: Joi.string().min(10).required(),
+    bio: Joi.string().min(3).required(),
+});
 
+const signInSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+});
+
+
+
+// export const createUserFn = async (
+//     email: string,
+//     password: string,
+//     username: string,
+//     firstName: string,
+//     lastName: string,
+//     phoneNumber: string,
+//     bio: string
+// ) => {
+//     // const { email, password, username, firstName, lastName, phoneNumber, bio } = props;
+//     try {
+//         // check if username or email is already in use
+//         const identicalEmailAccount = await prisma.user.findFirst({ where: { email } });
+//         const identicalUsernameAccount = await prisma.user.findFirst({ where: { username } });
+
+//         if (identicalEmailAccount?.email === email) {
+//             createHttpError(409, "Email already exists.");
+//             return;
+//         } else if (identicalUsernameAccount?.username === username) {
+//             createHttpError(409, "Username already exists.");
+//             return;
+//         }
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         if (!hashedPassword) {
+//             createHttpError(500, "Internal Server Error");
+//             return;
+//         }
+//         console.log(`EMAIL IS: ${email}`);
+//         console.log(`PASSWORD IS: ${password}`);
+//         console.log(`USERNAME IS: ${username}`);
+//         console.log(`FIRST NAME IS: ${firstName}`);
+//         console.log(`LAST NAME IS: ${lastName}`);
+//         console.log(`PHONE NUMBER IS: ${phoneNumber}`);
+//         console.log(`BIO IS: ${bio}`);
+//         console.log(`EMAIL IN USE IS: ${identicalEmailAccount?.email.toString()}`);
+//         console.log(`USERNAME IN USE IS: ${identicalUsernameAccount?.username}`);
+//         console.log(`HASHED PASSWORD IS: ${hashedPassword}`);
+
+//         const user = await prisma.user.create({
+//             data: {
+//                 email,
+//                 password: hashedPassword,
+//                 username,
+//                 firstName,
+//                 lastName,
+//                 phoneNumber,
+//                 bio,
+//             },
+//         });
+
+//         console.log(`PRISMA CREATED USER IS: ${user}`);
+//         return user;
+
+//     } catch (error: any) {
+//         console.log(error);
+//         createHttpError(500, "Internal Server Error");
+
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// };
+
+router.post('/signUp', async (req: Request, res: Response) => {
+    const { email, password, username, firstName, lastName, phoneNumber, bio } = req.body;
+    try {
+        if (signUpSchema.validate(req.body).error) {
+            createResponse(
+                {
+                    res: res,
+                    statusCode: 400,
+                    statusText: "BAD REQUEST",
+                    message: "Invalid data.",
+                    data: { error: `Invalid data: ${signUpSchema.validate(req.body).error}` },
+                }
+            );
+            return;
+        }
+        // console.log(`EMAIL IS: ${email}`);
+        // console.log(`PASSWORD IS: ${password}`);
+        // console.log(`USERNAME IS: ${username}`);
+        // console.log(`FIRST NAME IS: ${firstName}`);
+        // console.log(`LAST NAME IS: ${lastName}`);
+        // console.log(`PHONE NUMBER IS: ${phoneNumber}`);
+        // console.log(`BIO IS: ${bio}`);
+        // console.log(`USER IS: ${user}`)
+        // console.log(`USER IS: ${user}`);
+
+        // check if username or email is already in use
+        const identicalEmailAccount = await prisma.user.findFirst({ where: { email } });
+        const identicalUsernameAccount = await prisma.user.findFirst({ where: { username } });
+
+        if (identicalEmailAccount?.email === email) {
+            createResponse(
+                {
+                    res: res,
+                    statusCode: 409,
+                    statusText: "CONFLICT",
+                    message: "Email already exists.",
+                    data: { error: "Email already exists." },
+                }
+            );
+            return;
+        } else if (identicalUsernameAccount?.username === username) {
+            createResponse(
+                {
+                    res: res,
+                    statusCode: 409,
+                    statusText: "CONFLICT",
+                    message: "Username already exists.",
+                    data: { error: "Username already exists." },
+                }
+            );
+            return;
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        if (!hashedPassword) {
+            createResponse(
+                {
+                    res: res,
+                    statusCode: 500,
+                    statusText: "Internal Server Error",
+                    message: "Internal Server Error.",
+                    data: { error: "Internal Server Error." },
+                }
+            );
+            return;
+        }
         const user = await prisma.user.create({
             data: {
                 email,
@@ -33,32 +173,19 @@ export const createUserFn = async (props: CreateuserProps) => {
                 bio,
             },
         });
-
-        return user;
-    } catch (error: any) {
-        console.log(error);
-        createHttpError(500, "Internal Server Error");
-
-    } finally {
-        await prisma.$disconnect();
-    }
-};
-
-router.post('/signUp', async (req: Request, res: Response) => {
-    const { email, password, username, firstName, lastName, phoneNumber, bio } = req.body;
-
-    try {
-        const user = await createUserFn({ email, password, username, firstName, lastName, phoneNumber, bio });
-        const payload = { email: user?.email, password: user?.password };
-
+        const payload = { email: email, password: password };
         const jwtToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_EXPIRATION_TIME as string });
-
-        res.status(201).json({ user, jwtToken });
-
-
+        createResponse(
+            {
+                res: res,
+                statusCode: 201,
+                statusText: "CREATED",
+                message: "User created.",
+                data: { user, jwtToken },
+            }
+        );
     } catch (error: any) {
         console.log(error);
-
     } finally {
         await prisma.$disconnect();
     }
@@ -74,23 +201,31 @@ router.post('/signIn', async (req: Request, res: Response) => {
             },
         });
 
-        const payload = { email: user?.email, password: user?.password };
-
-        const jwtToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_EXPIRATION_TIME as string });
+        if (signInSchema.validate(req.body).error) {
+            createResponse(
+                {
+                    res: res,
+                    statusCode: 400,
+                    statusText: "BAD REQUEST",
+                    message: "Invalid data.",
+                    data: { error: `Invalid data: ${signInSchema.validate(req.body).error}` },
+                }
+            );
+            return;
+        }
 
         if (!user) {
             createResponse(
                 {
                     res: res,
                     statusCode: 401,
-                    statusText: "Unauthorized",
-                    message: "Invalid credentials.",
+                    statusText: "UNAUTHORIZED",
+                    message: "User does not exist.",
                     data: { error: "Invalid credentials." },
                 }
             );
             return;
         }
-
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (!passwordMatch) {
@@ -98,13 +233,20 @@ router.post('/signIn', async (req: Request, res: Response) => {
                 {
                     res: res,
                     statusCode: 401,
-                    statusText: "Unauthorized",
-                    message: "Invalid credentials.",
-                    data: { error: "Invalid credentials." },
+                    statusText: "UNAUTHORIZED",
+                    message: "Invalid password.",
+                    data: { error: "Invalid password." },
                 }
             );
             return;
         }
+
+
+        const payload = { email: email, password: password };
+
+        const jwtToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_EXPIRATION_TIME as string });
+
+
 
         createResponse(
             {
@@ -131,7 +273,15 @@ router.post('/signOut', async (req: Request, res: Response) => {
             }
         );
     } catch (error: any) {
-        createHttpError(500, "Internal Server Error");
+        createResponse(
+            {
+                res: res,
+                statusCode: 500,
+                statusText: "Internal Server Error",
+                message: "Internal Server Error.",
+                data: { error: `Internal Server Error: ${error}` },
+            }
+        );
         console.log(error);
     }
 });
@@ -140,74 +290,28 @@ router.post('/signOut', async (req: Request, res: Response) => {
 router.get('/users', async (req: Request, res: Response) => {
     try {
         const users = await prisma.user.findMany();
-        res.status(200).json(users);
-    } catch (error: any) {
-        console.log(error);
-    }
-});
 
-// create a user called "John Doe"
-router.post('/createJohnDoe', async (req: Request, res: Response) => {
-    try {
-        const { email } = req.body;
+        const count = await prisma.user.count();
+        // take out password from the response by using spread operator
 
-        // isEmailAlreadyInUse 
-        const user = await prisma.user.findUnique({
-            where: {
-                email,
-            },
-        });
-
-        if (user) {
-            createResponse(
-                {
-                    res: res,
-                    statusCode: 400,
-                    statusText: "Bad Request",
-                    message: "Email already in use.",
-                    data: { error: "Email already in use." },
-                }
-            );
-            return;
-        }
-        if (email === "" || email === undefined) {
-            res.status(400).json(
-                {
-                    error: "Please provide an email.",
-                    status: 400,
-                    statusText: "Bad Request"
-
-                }
-            );
-            return;
-        }
+        const usersObjectWithoutPassword = users.map(({ password, ...rest }) => rest);
 
 
-        const jwtPayload = { email: email, password: "password" };
-
-        const jwtToken = jwt.sign(jwtPayload, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_EXPIRATION_TIME as string });
-
-        const user1 = await createUserFn({
-            email: email,
-            password: "password",
-            username: "JohnDoe",
-            firstName: "John",
-            lastName: "Doe",
-            phoneNumber: "1234567890",
-            bio: "I am John Doe",
-        });
-        console.log({ user, jwtToken });
         createResponse(
             {
-                res: res,
-                statusCode: 201,
-                statusText: "Created",
-                message: "User created.",
-                data: { user: user1, jwtToken: jwtToken },
+                res,
+                statusCode: 200,
+                statusText: 'OK',
+                message: 'User found',
+                data: {
+                    count,
+                    users: usersObjectWithoutPassword
+                },
             }
         );
-    }
-    catch (error: any) {
+
+
+    } catch (error: any) {
         console.log(error);
     }
 });
@@ -229,7 +333,7 @@ router.put('/updateUser', async (req: Request, res: Response) => {
             },
         });
 
-        res.status(200).json(user);
+        // res.status(200).json(user);
     } catch (error: any) {
         console.log(error);
     }
